69a50ebf5853d46352b50fe75c573c9de64ccf62
ludwi <ludwig.lillieborg@frojd.se>
2021-05-17 21:12:34 +0200

    Add create-react-app

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import './App.scss';
+import '../../styling/index.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <h1 className="app__title">ludwig.lillieb.org</h1>
+    </div>
+  );
+}
+
+export default App;
