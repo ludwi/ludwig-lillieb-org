444e3e42caf1ec2406e711a52f1451f7c064af30
ludwi <ludwig.lillieborg@frojd.se>
2021-06-05 16:04:46 +0200

    Add animation to letters

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,7 +4,33 @@ import '../../styling/index.scss';
 function App() {
   return (
     <div className={s.app}>
-      <h1 className={s.app__title}>ludwig.lillieb.org</h1>
+      <h1 className={s.app__title}>
+        <span className={s.app__word}>
+          <span className={`${s.app__letter} ${s['app__letter--1']}`}>l</span>
+          <span className={`${s.app__letter} ${s['app__letter--2']}`}>u</span>
+          <span className={`${s.app__letter} ${s['app__letter--3']}`}>d</span>
+          <span className={`${s.app__letter} ${s['app__letter--4']}`}>w</span>
+          <span className={`${s.app__letter} ${s['app__letter--5']}`}>i</span>
+          <span className={`${s.app__letter} ${s['app__letter--6']}`}>g</span>
+        </span>
+        <span className={`${s.app__letter} ${s['app__letter--whitespace']}`}> </span>
+        <span className={`${s.app__letter} ${s['app__letter--7']}`}>.</span>
+        <span className={s.app__word}>
+          <span className={`${s.app__letter} ${s['app__letter--8']}`}>l</span>
+          <span className={`${s.app__letter} ${s['app__letter--9']}`}>i</span>
+          <span className={`${s.app__letter} ${s['app__letter--10']}`}>l</span>
+          <span className={`${s.app__letter} ${s['app__letter--11']}`}>l</span>
+          <span className={`${s.app__letter} ${s['app__letter--12']}`}>i</span>
+          <span className={`${s.app__letter} ${s['app__letter--13']}`}>e</span>
+          <span className={`${s.app__letter} ${s['app__letter--14']}`}>b</span>
+        </span>
+        <span className={`${s.app__letter} ${s['app__letter--15']}`}>.</span>
+        <span className={s.app__word}>
+          <span className={`${s.app__letter} ${s['app__letter--16']}`}>o</span>
+          <span className={`${s.app__letter} ${s['app__letter--17']}`}>r</span>
+          <span className={`${s.app__letter} ${s['app__letter--18']}`}>g</span>
+        </span>
+      </h1>
     </div>
   );
 }
