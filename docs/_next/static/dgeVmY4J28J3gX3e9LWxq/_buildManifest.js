0eb81ec5772db3a199b89d44260c3bd9a0055f21
ludwi <ludwig.lillieborg@frojd.se>
2025-11-26 13:54:29 +0100

    Add Next.js build files and .nojekyll

---
+++ docs/_next/static/dgeVmY4J28J3gX3e9LWxq/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/524a9fa2017d9a26.css","static/chunks/pages/index-c9a0b9a306cf91f4.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
